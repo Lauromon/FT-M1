57c70ba47336121016a5845d880df3dcfd73cc02
Lautaro <orbesle@gmail.com>
2022-09-21 10:13:20 -0300

    ok

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -4,24 +4,25 @@
 function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
-  // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
+  // Ej: factorear(180) --> [1,2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
   //tengo que devolver un array con los nros por los que pude dividir.
   //el factor tiene que guardarse y aumentar con cada vuelta.
   //hay una condicion, no puede ser dividido por un nro cuyo resultado sea un decimal. 
-  var arr = [];
-  var factor = 0;
-  if(num % factor === 0) {
-    factor ++;
-    return factorear(num);
+  var arr = [1];
+  var factor = 2;
+  while(num > 1){
+    if(num % factor === 0 ) {
+      arr.push(factor);
+      num = num / factor;
+    }
+    else{
+      factor ++;
+    }
   }
-  else{ 
-    num = num / factor;
-    arr.push(factor);
-    factor++;
-    return factorear(num);
-  }
+  return arr;
 }
+
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
@@ -29,10 +30,11 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
   /*tengo que:
-    comparar elementos dentro del array con el que le sigue
-    intercambiar sus lugares
-    si el elemento que le sigue es mayor, queda como esta, si es menor, cambian de lugar.
-    recursivo.*/
+  comparar elementos dentro del array con el que le sigue
+  intercambiar sus lugares
+  si el elemento que le sigue es mayor, queda como esta, si es menor, cambian de lugar.
+  recursivo.*/
+  
   for(var i = 0; i < array.length; i++){
     for(var j = i + 1; j < array.length; j++){
       if(array[i] > array[j]){
@@ -45,35 +47,50 @@ function bubbleSort(array) {
   return array;
 }
 
+  
 function insertionSort(array) {
-  // Implementar el método conocido como insertionSort para ordenar de menor a mayor
-  // el array recibido como parámetro utilizando arreglos
-  // Devolver el array ordenado resultante
+    // Implementar el método conocido como insertionSort para ordenar de menor a mayor
+    // el array recibido como parámetro utilizando arreglos
+    // Devolver el array ordenado resultante
   // Tu código:
   /*tengo que:
     extraer un elemento del array y compararlo con el resto para luego ubicarlo donde iria, con cada un*/
-    var comparado = []
-    for(var i = 0; i < array.length; i++){
-      comparado.push(array[i]);
-      if(comparado)
+  for (let i = 1; i < array.length; i++){
+    var j = i -1;
+    var aux = array[i];
+    while (j >= 0 && array[j]> aux){
+      array[j+1] = array[j];
+      j = j - 1;
     }
-    return array;
+    array[j+1] = aux;
   }
-  /*for(var j = i + 1; j < array.length; j++){
-    if(array[i] > array[j]){
-      var mayor = array[i];
-      array[i] = array[j];
-      array[j] = mayor;
-    }
-  }*/
-
-
+  return array
+}
+  
+  
+  
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+        var min = i;
+    
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      var aux = array[i];
+      array[i] = array[min];
+      array[min] = aux;
+      //[array[i], array[min]] = [array[min], array[i]];
+    }
+  }
 
+  return array;
 }
 
 
