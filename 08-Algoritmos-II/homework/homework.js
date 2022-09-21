95cf611a87d807eb0402f476492b3bb78903f422
Lautaro <orbesle@gmail.com>
2022-09-21 13:46:49 -0300

    ok

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils");
+
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,7 +9,27 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+/* necesito un pivote (un elemento del array)
+  separar el array en dos(menores y mayores al pivote)
+  comparar con el pivote todos los elementos menores y luego con los del array mayores
+  concatenar todo en un nuevo array ordenado*/
+  var menores = [];
+  var mayores = [];
+  var pivote = array[0];
 
+  if(array.length < 1){
+    return [];
+  }
+
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] < pivote){
+      menores.push(array[i]);
+    }
+    else{
+      mayores.push(array[i]);
+    }
+  }
+  return [].concat(quickSort(menores), pivote, quickSort(mayores));
 }
 
 function mergeSort(array) {
@@ -14,7 +37,28 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length < 2){
+    return array
+  }
 
+  var medio = Math.floor(array.length/2)
+  var izq = array.slice(0,medio);
+  var der = array.slice(medio);
+  
+  var merge = function(izq, der) {
+    var datos = [];
+    
+    while(izq.length > 0 && der.length > 0){
+      if(izq[0] <= der[0]){
+        datos.push(izq.shift());
+      }
+      else{
+        datos.push(der.shift());
+      }
+    }
+    return datos.concat(izq).concat(der);
+  }
+  return merge(mergeSort(izq),mergeSort(der));
 }
 
 // No modificar nada debajo de esta línea
